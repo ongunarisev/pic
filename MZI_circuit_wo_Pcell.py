cab3f4e47a08ab302e6731b7429672b69804f92a
Ongun Arisev (desktop) <ongunarisev@gmail.com>
2023-05-05 17:53:31 +0200

    Y-branches all circuits combined
    + Problems with connections
    + Waveguide length needs to be set

--- MZI_circuit_wo_Pcell.py
+++ MZI_circuit_wo_Pcell.py
@@ -6,6 +6,11 @@ from ipkiss3 import all as i3
 import pylab as plt
 
 ### PARAMETERS
+
+# Location of the bottom left grating
+x_gc = 50
+y_gc = 20
+
 # The fiber spacing
 f_spacing = 127  # microns
 n_instances = 3  # We use a total of 3 fibers
@@ -24,7 +29,7 @@ fgc = pdk.EbeamGCTE1550()
 insts = {"{0}_{1:02d}".format(name, i): fgc for i, name in enumerate(n_instances * ["fgc"], start=1)}
 
 # 5. We define specs, containing all the transformations that apply to each component.
-specs = [i3.Place("{}:opt1".format(key), (0, 0 + i * f_spacing)) for i, key in enumerate(insts.keys())]
+specs = [i3.Place("{}:opt1".format(key), (x_gc, y_gc + i * f_spacing)) for i, key in enumerate(insts.keys())]
 
 # 6. We define the names of the exposed ports that we want to access.
 exposed_port_names = {f"{key}:opt1":f"gc_{i:02d}" for i, key in enumerate(insts.keys(), start=1)}
@@ -52,11 +57,11 @@ floorplan = pdk.FloorPlan(name="FLOORPLAN", size=(605.0, 410.0))
 # Connect the Y-branch to the input grating
 insts = {
     "yb_1": y_branch,
-    "grating_array":  fgcirc
+    "fgcirc":  fgcirc
 }
 
 specs = [
-    i3.Join("grating_array:gc_02", "yb_1:opt1"),
+    i3.Join(f"fgcirc:gc_{input_port:02d}", "yb_1:opt1"),
 ]
 
 
@@ -115,3 +120,61 @@ fgcirc_y2 = i3.Circuit(
 # This is the third stage of the circuit
 fgcirc_y2_layout = fgcirc_y2.Layout()
 fgcirc_y2_layout.visualize(annotate=True)
+
+# Now moving onto the fourth stage putting the Y-branches acting as combiners and setting the path length difference
+insts = {
+    "fgcirc_y2": fgcirc_y2,
+    "yb_4": y_branch,
+    "yb_5": y_branch,
+}
+
+specs = [
+    i3.ConnectManhattan("fgcirc_y2:yb2_1", "yb_4:opt3"),
+    i3.ConnectManhattan("fgcirc_y2:yb2_2", "yb_4:opt2"),
+    i3.ConnectManhattan("fgcirc_y2:yb3_1", "yb_5:opt3"),
+    i3.ConnectManhattan("fgcirc_y2:yb3_2", "yb_5:opt2"),
+    i3.PlaceRelative("yb_4:opt1", "fgcirc_y2:yb2_1", (2.5 * x_spacing, y_spacing*3)),
+    i3.PlaceRelative("yb_5:opt1", "fgcirc_y2:yb3_2", (2.5 * x_spacing, -y_spacing*3)),
+    i3.FlipH("yb_4"),
+    i3.FlipH("yb_5")
+]
+
+exposed_port_names = {
+    "yb_4:opt1": "out_mzi_1",
+    "yb_5:opt1": "out_mzi_2"
+}
+
+fgcirc_y3 = i3.Circuit(
+    name="GCs_ybranch3",
+    insts=insts,
+    specs=specs,
+    exposed_ports=exposed_port_names,
+)
+
+# This is the fourth stage of the circuit
+fgcirc_y3_layout = fgcirc_y3.Layout()
+fgcirc_y3_layout.visualize(annotate=True)
+
+# Moving onto the fifth and last stage of the circuit
+
+insts = {
+    "fgcirc_y3": fgcirc_y3,
+}
+
+specs = [
+    i3.ConnectManhattan("fgcirc_y3:out_mzi_1", "fgcirc_y3:gc_02"),
+    i3.ConnectManhattan("fgcirc_y3:out_mzi_2", "fgcirc_y3:gc_01"),
+]
+
+exposed_port_names = {}
+
+fgcirc_y4 = i3.Circuit(
+    name="GCs_ybranch_final",
+    insts=insts,
+    specs=specs,
+    exposed_ports=exposed_port_names,
+)
+
+# This is the fourth stage of the circuit
+fgcirc_y4_layout = fgcirc_y4.Layout()
+fgcirc_y4_layout.visualize(annotate=True)
