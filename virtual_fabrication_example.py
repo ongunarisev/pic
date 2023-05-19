ef8ddde9d46c56dc1b8020d89cff26fbc7f9b43c
Ongun Arisev (desktop) <ongunarisev@gmail.com>
2023-05-19 19:59:11 +0200

    Intermediate commit

--- virtual_fabrication_example.py
+++ virtual_fabrication_example.py
@@ -11,7 +11,10 @@ class Donut(i3.PCell):
         def _generate_elements(self, elems):
             elems += i3.Circle(layer=i3.Layer(0), radius=self.radius)
             elems += i3.Circle(layer=i3.Layer(1), radius=self.inner_radius)
+            elems += i3.Rectangle(layer=i3.Layer(2), center=(-10, 0), box_size=(1, 1))
+            elems += i3.Rectangle(layer=i3.Layer(2), center=(10, 0), box_size=(1, 1))
             return elems
+
 
 # instantiate the cell and visualize its layout
 donut = Donut(radius=15.0, inner_radius=5.0)
@@ -24,11 +27,12 @@ donut_lo.visualize()
 from pysics.basics.material.material import Material
 from pysics.basics.material.material_stack import MaterialStack
 from ipkiss.visualisation.display_style import DisplayStyle
-from ipkiss.visualisation.color import COLOR_SCARLET, COLOR_BLUE
+from ipkiss.visualisation.color import *
 from ipkiss.plugins.vfabrication.process_flow import VFabricationProcessFlow
 
 oxide = Material(name="oxide", display_style=DisplayStyle(color=COLOR_SCARLET))
 air = Material(name="air", display_style=DisplayStyle(color=COLOR_BLUE))
+metal = Material(name="metal", display_style=DisplayStyle(color=COLOR_SILVER))
 
 stack_oxide = MaterialStack(name="oxide slab",
                             materials_heights=[
@@ -48,24 +52,36 @@ stack_air = MaterialStack(name="all air",
                             display_style=DisplayStyle(color=COLOR_BLUE)
                             )
 
+stack_metal = MaterialStack(name="metal",
+                                materials_heights=[
+                                    (metal, 0.2),
+                                    (air, 0.3),
+                                    (oxide, 0.5),
+                                    (air, 1.0),
+                                ])
+
 oxide_dep = i3.ProcessLayer(name="oxide", extension="OX")
 oxide_etch = i3.ProcessLayer(name="oxide etch", extension="NOX")
+metalization = i3.ProcessLayer(name="metalization", extension="M")
 
 vfab = VFabricationProcessFlow(
-    active_processes=[oxide_dep, oxide_etch],
+    active_processes=[oxide_dep, oxide_etch, metalization],
     process_layer_map={
         oxide_dep: i3.Layer(0),
-        oxide_etch: i3.Layer(1)
+        oxide_etch: i3.Layer(1),
+        metalization: i3.Layer(2),
     },
     is_lf_fabrication={
         oxide_dep: False,
-        oxide_etch: False
+        oxide_etch: False,
+        metalization: False,
     },
     process_to_material_stack_map=[
-        ((0, 0), stack_air),
-        ((0, 1), stack_air),
-        ((1, 0), stack_oxide),
-        ((1, 1), stack_air)
+        ((0, 0, 0), stack_air),
+        ((0, 1, 0), stack_air),
+        ((1, 0, 0), stack_oxide),
+        ((1, 1, 0), stack_air),
+        ((0, 0, 1), stack_metal)
     ]
 )
 
