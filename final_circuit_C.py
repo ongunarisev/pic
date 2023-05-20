b4248c1ce93165be8099ca74d499b58116da2d1d
Ongun Arisev (desktop) <ongunarisev@gmail.com>
2023-05-20 17:33:11 +0200

    Final Circuit C data saved
    + Model data saved from C:\luceda\luceda_academy_39-111\libraries\pteam_library_siepic\ipkiss\pteam_library_siepic\components\grating_unit_cell\model_data
    + Final plot data saved in MATLAB and pickle format

--- final_circuit_C.py
+++ final_circuit_C.py
@@ -6,11 +6,15 @@ from ipkiss3 import all as i3
 from ipkiss3 import constants
 from ipkiss.process.layer_map import GenericGdsiiPPLayerOutputMap
 from pteam_library_siepic.all import UnitCellRectangular, UniformWBG
+from pteam_library_siepic.components.grating_unit_cell.fitting import create_uc_model_data
 from wbg_cell import WBGCircuit
 
 from datetime import datetime
+from scipy.io import savemat, loadmat
+import matplotlib.gridspec as gridspec
 import numpy as np
 import pylab as plt
+import pickle
 
 # We make a copy of the layer dictionary to freely modify it
 pplayer_map = dict(i3.TECH.GDSII.LAYERTABLE)
@@ -92,6 +96,10 @@ meas_label_coord = wbg_circuit2.measurement_label_position + (x0, y0)
 text_label_dict[wbg_circuit_name2] = [meas_label, meas_label_coord]
 circuit_cell_names.append(wbg_circuit_name2)
 
+# Create models for different unit cells if necessary
+# create_uc_model_data(uc_pcell=uc1)  # This is the default cell
+# create_uc_model_data(uc_pcell=uc2)  # This is the cell with smaller features
+
 
 # Create the final design with i3.Circuit
 top_cell = i3.Circuit(
@@ -122,9 +130,63 @@ cell_lv.visualize(annotate=True)
 cell_lv.visualize_2d()
 cell_lv.write_gdsii(filename, layer_map=output_layer_map)
 
-# Circuit model
-cell_cm = top_cell.CircuitModel()
-wavelengths = np.linspace(1.52, 1.58, 4001)
-S_total = cell_cm.get_smatrix(wavelengths=wavelengths)
+
+########################################################################################################################
+# Simulate the circuits
+########################################################################################################################
+
+for wbg in [wbg1, wbg2]:
+
+    temperatures = np.linspace(273, 373, 3)
+    wavelengths = np.arange(1.50, 1.60, 0.0001)
+
+    fig = plt.figure(constrained_layout=True)
+    gs = gridspec.GridSpec(1, 2, figure=fig)
+    axes_sparam = [fig.add_subplot(gs[0, 0]), fig.add_subplot(gs[0, 1])]
+
+    axes_sparam[0].set_xlabel(r"Wavelength (nm)", fontsize=16)
+    axes_sparam[0].set_ylabel("WBG Reflection (a.u.)", fontsize=16)
+    axes_sparam[0].set_title("Raw data", fontsize=20)
+
+    axes_sparam[1].set_xlabel(r"Wavelength (nm)", fontsize=16)
+    axes_sparam[1].set_ylabel("WBG Reflection (a.u.)", fontsize=16)
+    axes_sparam[1].set_title("Normalised data", fontsize=20)
+
+    for temperature in temperatures:
+
+        wbg.CircuitModel(temperature=temperature)
+        full_circuit = WBGCircuit(wbg=wbg)
+
+        full_circuit_cm = full_circuit.CircuitModel()
+        full_circuit_Smatrix = full_circuit_cm.get_smatrix(wavelengths=wavelengths)
+        reflection = i3.signal_power(full_circuit_Smatrix["in:0", "wbg_r:0"])
+        transmission_ref = i3.signal_power(full_circuit_Smatrix["in:0", "ref:0"])
+
+        axes_sparam[0].plot(
+            wavelengths * 1000,
+            reflection,
+            "o-",
+            linewidth=2.2,
+            label=r"Reflection at " + str(int(temperature - 273)) + r" $^{\circ}C$",
+        )
+        axes_sparam[1].plot(
+            wavelengths * 1000,
+            reflection / transmission_ref,
+            "o-",
+            linewidth=2.2,
+            label=r"Reflection at " + str(int(temperature - 273)) + r" $^{\circ}C$",
+        )
+
+    axes_sparam[0].legend(fontsize=14, loc=6)
+    axes_sparam[1].legend(fontsize=14, loc=6)
+    axes_sparam[0].tick_params(which="both", labelsize=14)
+    axes_sparam[1].tick_params(which="both", labelsize=14)
+    m_dict = {"wavelength":wavelengths, "reflection":reflection,
+              "transmission_ref":transmission_ref, "full_circuit_Smatrix":full_circuit_Smatrix}
+    savemat(f'./data/wbg_w_{wbg.uc.width}_dw_{wbg.uc.deltawidth}_l1_{wbg.uc.length1}_l2_{wbg.uc.length2}.mat', m_dict)
+    with open(f'./data/wbg_w_{wbg.uc.width}_dw_{wbg.uc.deltawidth}_l1_{wbg.uc.length1}_l2_{wbg.uc.length2}.pkl', 'wb') as f:  # Python 3: open(..., 'wb')
+        pickle.dump([wavelengths, reflection, transmission_ref, full_circuit_Smatrix], f)
+        # pickle.load(open('./data/wbg_w_0.5_dw_0.089_l1_0.26_l2_0.065.pkl', 'rb'))  # Example command to read
+plt.show()
 
 print("Done")
